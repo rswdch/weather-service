dccc3e9bccf5b39f0f3ebcc17707062c1f5ba71c
rswdch <ryan.sawadichai@outlook.com>
2024-05-19 18:39:35 -0600

    global error handler and 404 handler

--- openweather-api-node/src/index.ts
+++ openweather-api-node/src/index.ts
@@ -1,15 +1,61 @@
-import express, { Request, Response } from 'express';
-import { logger } from './logger/WinstonLogger';
+import express, { Request, Response, NextFunction } from "express";
+import { logger } from "./logger/WinstonLogger";
+import * as expressWinston from "express-winston";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (_req: Request, res: Response) => {
-  logger.info('this is an info log');
-  logger.debug('this is a debug log from /');
+app.use(
+  expressWinston.logger({
+    winstonInstance: logger,
+    // statusLevels: true,
+    level: 'http',
+  }),
+);
+
+app.get("/", (_req: Request, res: Response) => {
+  logger.info("this is an info log");
+  logger.debug("this is a debug log from /");
   res.send("Hello world").status(200).end();
+});
+
+app.get("/error", () => {
+  throw new Error("custom error");
+});
+
+// Express Winston error logger middleware
+app.use(
+  expressWinston.errorLogger({
+    winstonInstance: logger,
+    level: 'error',
+  }),
+);
+
+// 404 error handler
+app.use((req, res, next) => {
+  res.status(404).send("Error 404: Page not found.")
 })
+
+// global error handler
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction): void => {
+  const defaultError = {
+    log: "Express error handler caught unknown middleware error",
+    status: 500,
+    message: { err: "An error occurred" },
+  };
+
+  // add custom error message and stack trace if available
+  const errorObj = {
+    ...defaultError,
+    ...(err instanceof Error ? { message: { err: err.message }, stack: err.stack } : err),
+  };
+
+  logger.debug(errorObj.stack);
+
+  res.status(errorObj.status).json(errorObj.message);
+});
+
 
 app.listen(PORT, () => {
   logger.info(`Listening on port ${PORT}`);
-})
+});
